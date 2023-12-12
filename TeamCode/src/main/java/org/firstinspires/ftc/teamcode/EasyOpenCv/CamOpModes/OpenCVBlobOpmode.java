21cc00ffed260b7bd1e9a15edf1882e32147d006
ethanCouch <117788040+ethanCouch@users.noreply.github.com>
2023-12-12 17:05:54 -0500

    Added FieldCentricGameTeleOp but haven't tested it
    Field centric should work but I haven't tested It, I am still making progress on blob detection so you may need to comment it out before you build the program.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EasyOpenCv/CamOpModes/OpenCVBlobOpmode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EasyOpenCv/CamOpModes/OpenCVBlobOpmode.java
@@ -1,38 +1,133 @@
 package org.firstinspires.ftc.teamcode.EasyOpenCv.CamOpModes;
 
+
+import static org.opencv.features2d.Features2d.DrawMatchesFlags_DRAW_RICH_KEYPOINTS;
+import static org.opencv.features2d.Features2d.drawKeypoints;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.opencv.core.Core;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.opencv.core.MatOfKeyPoint;
+import org.opencv.core.Scalar;
 import org.opencv.features2d.SimpleBlobDetector;
 import org.opencv.core.Mat;
+import org.opencv.features2d.SimpleBlobDetector_Params;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
 import org.openftc.easyopencv.OpenCvPipeline;
 import org.openftc.easyopencv.OpenCvWebcam;
 
+// Useful documentation: https://docs.opencv.org/3.4/javadoc/org/opencv/features2d/SimpleBlobDetector.html#create()
+
+// stack overflow: https://stackoverflow.com/questions/41116049/how-can-i-use-blob-detector#:~:text=To%20detect%20blobs%20with%20OpenCV%20you%20need%20to%3A,of%20type%20KeyPoint%20s%203%20Call%20SimpleBlobDetector%3A%3Adetect%20%28%29
+
+/* object dimentions:
+          ______
+ 2-4 in  |      |
+         |      |
+         |______|
+ 2-4 in
+
+*/
+@Autonomous(name="SimpleOpenCVBlobOpMode")
 public class OpenCVBlobOpmode extends OpMode {
+
+
 
     //Resolution
     static final int STREAM_WIDTH = 1280; // modify for your camera
     static final int STREAM_HEIGHT = 720; // modify for your camera
-    OpenCvWebcam webcam;
-    OpenCVBlobPipeline pipeline;
+    OpenCvWebcam webcam; 
+    SimpleOpenCVBlobPipeline pipeline;
 
     @Override
     public void init() {
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        WebcamName webcamName = null;
+        webcamName = hardwareMap.get(WebcamName.class, "Webcam 1"); // put your camera's name here
+        webcam = OpenCvCameraFactory.getInstance().createWebcam(webcamName, cameraMonitorViewId);
+        pipeline = new SimpleOpenCVBlobPipeline();
+        webcam.setPipeline(pipeline);
+        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
+            @Override
+            public void onOpened()
+            {
+                webcam.startStreaming(STREAM_WIDTH, STREAM_HEIGHT, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode) {
+                telemetry.addData("Camera Failed","L");
+                telemetry.update();
+            }
+        });
 
     }
 
     @Override
     public void loop() {
 
+
+// Show blobs
+        telemetry.addData("keypoints:", pipeline.getBlobAnalysis());
+        // Display the image with keypoints
+        Core.imshow("Keypoints", im);
+        Core.waitKey(0);
+
     }
 }
 
-class OpenCVBlobPipeline extends OpenCvPipeline {
-    Mat blob;
-
+class SimpleOpenCVBlobPipeline extends OpenCvPipeline {
+    Mat BlobImg;
+    MatOfKeyPoint keypoints;
+    Mat im_with_keypoints;
+    SimpleBlobDetector_Params params;
     @Override
     public Mat processFrame(Mat input) {
-        SimpleBlobDetector.create();
 
-        System.out.println("processing requested");
-        return null;
+        try {
+// Read image
+            BlobImg = Imgcodecs.imread("blob.jpg", Imgcodecs.IMREAD_GRAYSCALE);
+
+            Imgproc.cvtColor(input, BlobImg, Imgproc.COLOR_RGB2GRAY);
+
+//      Set min/max thresholds:
+            params.set_minThreshold(50);
+            params.set_maxThreshold(100);
+
+//      Filter by min/max area:
+            params.set_minArea(2);
+//        params.set_maxArea(4);
+
+// Create a detector with the parameters
+// OLD: detector = cv2.SimpleBlobDetector(params)
+            SimpleBlobDetector detector = SimpleBlobDetector.create(params);
+
+            keypoints = detector.detect(BlobImg);
+
+            SimpleBlobDetector.create(params).detect(BlobImg, keypoints);
+
+//      Draw detected blobs as red circles.
+//      DrawMatchesFlags::DRAW_RICH_KEYPOINTS flag ensures the size of the circle corresponds to the size of blob
+            drawKeypoints(BlobImg, keypoints, im_with_keypoints, new Scalar(0, 0, 225), DrawMatchesFlags_DRAW_RICH_KEYPOINTS);
+
+            System.out.println("processing requested");
+
+
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        return input;
     }
+
+    public Mat getBlobAnalysis() {return im_with_keypoints;}
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/FieldCentricGameTeleOp.java
@@ -0,0 +1,133 @@
+package org.firstinspires.ftc.teamcode.drive.opmode;
+
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
+    game plan!
+    Need encoders for linear slide + code for them
+    Need to test all systems once robot is (theoretically) functioning
+*/
+
+
+        import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+        import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+        import com.qualcomm.robotcore.hardware.DcMotor;
+        import com.qualcomm.robotcore.hardware.DcMotorSimple;
+        import com.qualcomm.robotcore.util.ElapsedTime;
+        import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.teamcode.drive.opmode.subsystems.slideOpmode;
+
+@TeleOp(name="Game Opmode", group="Linear Opmode")
+//@Disabled
+public class FieldCentricGameTeleOp extends LinearOpMode {
+
+    // Declare OpMode members.
+    private ElapsedTime runtime = new ElapsedTime();
+    //drive motors
+    private DcMotor leftFrontDrive = null;
+    private DcMotor rightFrontDrive = null;
+    private DcMotor leftRearDrive = null;
+    private DcMotor rightRearDrive = null;
+
+
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        // Retrieve the IMU from the hardware map
+        IMU imu = hardwareMap.get(IMU.class, "imu");
+        // Adjust the orientation parameters to match your robot
+        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD));
+        // Without this, the REV Hub's orientation is assumed to be logo up / USB forward
+        imu.initialize(parameters);
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        while (opModeIsActive()) {
+            double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
+            double x = gamepad1.left_stick_x;
+            double rx = gamepad1.right_stick_x;
+
+            double slide = gamepad1.right_trigger;
+            double lowerslide = gamepad1.left_trigger;
+
+            // This button choice was made so that it is hard to hit on accident,
+            // it can be freely changed based on preference.
+            // The equivalent button is start on Xbox-style controllers.
+            if (gamepad1.options) {
+                imu.resetYaw();
+            }
+
+            double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+
+            // Rotate the movement direction counter to the bot's rotation
+            double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
+            double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
+
+            rotX = rotX * 1.1;  // Counteract imperfect strafing
+
+            // Denominator is the largest motor power (absolute value) or 1
+            // This ensures all the powers maintain the same ratio,
+            // but only if at least one is out of the range [-1, 1]
+            double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
+            double frontLeftPower = (rotY + rotX + rx) / denominator;
+            double backLeftPower = (rotY - rotX + rx) / denominator;
+            double frontRightPower = (rotY - rotX - rx) / denominator;
+            double backRightPower = (rotY + rotX - rx) / denominator;
+
+            double slidePower = (slide - lowerslide); //change speed multiplier after testing
+
+            leftFrontDrive.setPower(frontLeftPower); //
+            leftRearDrive.setPower(backLeftPower); //.
+            rightFrontDrive.setPower(frontRightPower); //drive forward slowly and keep straight.
+            rightRearDrive.setPower(backRightPower); //
+
+
+            //e.toString());
+            //%.2f)", leftPower, rightPower);
+            //
+        }
+    }
+}
+
